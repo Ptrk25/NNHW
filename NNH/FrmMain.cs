509a6cf7b3f71f59a9be648e2375453477d371a3
Patrick L <lbpatrick25@gmail.com>
2018-01-27 18:22:11 +0100

    ui update

--- NNH/FrmMain.cs
+++ NNH/FrmMain.cs
@@ -4,16 +4,47 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows.Forms;
 
 namespace NNH
 {
-    public partial class FrmMain : Form
+    public partial class FrmMain : Form, IMessageFilter
     {
+        // Notwendige Variablen zum verschieben des Fensters
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        public const int WM_LBUTTONDOWN = 0x0201;
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private HashSet<Control> controlsToMove = new HashSet<Control>();
+
+
+
         public FrmMain()
         {
             InitializeComponent();
+            Application.AddMessageFilter(this);
+            controlsToMove.Add(this);
+            controlsToMove.Add(this.pnlTitlebar);
+        }
+
+        // Fensterverschiebungscode
+        public bool PreFilterMessage(ref Message m)
+        {
+            if (m.Msg == WM_LBUTTONDOWN &&
+            controlsToMove.Contains(Control.FromHandle(m.HWnd)))
+            {
+                ReleaseCapture();
+                SendMessage(this.Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+                return true;
+            }
+            return false;
         }
 
         private void button1_Click(object sender, EventArgs e)
