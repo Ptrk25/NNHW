4b8cbbdafe3f602090a16c56b12983e790b9f10a
jakoblh <jakob.lamberthartmann.canada@gmail.com>
2018-01-28 11:41:55 +0100

    nothing new

--- NNH/NeuralNetwork.cs
+++ NNH/NeuralNetwork.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using MathNet.Numerics.LinearAlgebra;
 
 namespace NNH
 {
     class NeuralNetwork
     {
-        Vector<float> _input;
-        List<Matrix<float>> _weights;
+
     }
 }
