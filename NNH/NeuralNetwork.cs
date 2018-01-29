3deb1c5c056c4496759afbe5498c88d5382b077c
jakoblh <jakob.lamberthartmann.canada@gmail.com>
2018-01-29 20:55:15 +0100

    nnh init function creates all matricies

--- NNH/NeuralNetwork.cs
+++ NNH/NeuralNetwork.cs
@@ -5,6 +5,21 @@ namespace NNH
 {
     class NeuralNetwork
     {
+        List<Matrix<float>> _weights;
+        List<Matrix<float>> _biases;
+        Matrix<float> result;
 
+        public void init(List<int> layers, int output_size){
+            int prev_layer = layers[0];
+            for(int i = 1; i < layers.Count; i++){
+                _weights.Add(CreateMatrix.Dense<float>(prev_layer, layers[i]));
+                _biases.Add(CreateMatrix.Dense<float>(layers[i], 1));
+
+                prev_layer = layers[i];
+            }   
+            
+            _weights.Add(CreateMatrix.Dense<float>(prev_layer, output_size));
+            _biases.Add(CreateMatrix.Dense<float>(output_size, 1));
+        }
     }
 }
