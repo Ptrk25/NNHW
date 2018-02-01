bba00c4d41e7f0643a09e65685b04a2a3ae140d9
Patrick L <lbpatrick25@gmail.com>
2018-02-01 15:44:04 +0100

    MNIST Parser added

---
+++ NNH/MNISTImage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace NNH
+{
+    class MNISTImage
+    {
+
+        private int width, height;
+        private int[,] pixels;
+        public int label;
+
+        public MNISTImage(int width, int height, int label)
+        {
+            this.width = width;
+            this.height = height;
+            this.label = label;
+            pixels = new int[width, height];
+        }
+
+        public void setPixel(int intensity, int x, int y)
+        {
+            pixels[x, y] = intensity;
+        }
+    
+        public int getPixel(int x, int y)
+        {
+            return pixels[x, y];
+        }
+
+        public Bitmap getImageAsBitmap()
+        {
+            Bitmap image = new Bitmap(width, height);
+
+            for(int x = 0; x < width; x++)
+            {
+                for(int y = 0; y < height; y++)
+                {
+                    int intensity = pixels[x, y];
+                    image.SetPixel(x, y, Color.FromArgb(0, intensity, intensity, intensity));
+                }
+            }
+
+            return image;
+        }
+
+    }
+}
---
+++ NNH/MNISTParser.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace NNH
+{
+    class MNISTParser
+    {
+        private readonly int LABEL_MAGIC = 2049;
+        private readonly int IMAGES_MAGIC = 2051;
+
+        private string imagesPath, labelsPath;
+        private byte[] fileImages, fileLabels;
+        private List<MNISTImage> mnistImages;
+
+        public MNISTParser(string imagesPath, string labelsPath)
+        {
+            this.imagesPath = imagesPath;
+            this.labelsPath = labelsPath;
+        }
+
+        public bool parseMNIST()
+        {
+            bool success = true;
+            fileLabels = File.ReadAllBytes(labelsPath);
+            fileImages = File.ReadAllBytes(imagesPath);
+
+            success = parseLabels();
+            success = parseImages();
+
+            return success;
+        }
+
+        private bool parseLabels()
+        {
+            // Check MAGIC
+            if (BitConverter.ToInt32(fileLabels, 0) != LABEL_MAGIC)
+                return false;
+
+            int entries = BitConverter.ToInt32(fileLabels, 4);
+
+            for (int i = 8; i < fileLabels.Length; i++)
+            {
+                MNISTImage num_image = new MNISTImage(28, 28, fileLabels[i]);
+                mnistImages.Add(num_image);
+            }
+
+            if (entries != mnistImages.Count)
+                return false;
+
+            return true;
+        }
+
+        private bool parseImages()
+        {
+            // Check MAGIC
+            if (BitConverter.ToInt32(fileImages, 0) != IMAGES_MAGIC)
+                return false;
+
+            int entries = BitConverter.ToInt32(fileImages, 4);
+            int x, y, image;
+
+            if (entries != mnistImages.Count)
+                return false;
+
+            for (int i = 16; i < fileImages.Length; i++)
+            {
+                MNISTImage
+
+                x = i % 16;
+
+            }
+
+            return true;
+        }
+
+
+    }
+}
